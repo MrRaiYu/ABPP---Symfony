88928f6aa60e797495cf88dccc728053dfea577e
Melvin Aubril <maubril28@gmail.com>
2023-03-21 15:25:35 +0100

    Login Page

---
+++ ABPP_Symfony/src/Entity/Utilisateur.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UtilisateurRepository::class)
+ */
+class Utilisateur
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $UtilLogin;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $UtilMDP;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Profil::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $Profil;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUtilLogin(): ?string
+    {
+        return $this->UtilLogin;
+    }
+
+    public function setUtilLogin(string $UtilLogin): self
+    {
+        $this->UtilLogin = $UtilLogin;
+
+        return $this;
+    }
+
+    public function getUtilMDP(): ?string
+    {
+        return $this->UtilMDP;
+    }
+
+    public function setUtilMDP(string $UtilMDP): self
+    {
+        $this->UtilMDP = $UtilMDP;
+
+        return $this;
+    }
+
+    public function getProfil(): ?Profil
+    {
+        return $this->Profil;
+    }
+
+    public function setProfil(?Profil $Profil): self
+    {
+        $this->Profil = $Profil;
+
+        return $this;
+    }
+}
---
+++ ABPP_Symfony/src/Formulaire/ConnexionType.php
@@ -0,0 +1,18 @@
+<?php
+    namespace App\Formulaire;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+    class ConnexionType extends AbstractType
+    {
+        function buildForm(FormBuilderInterface $builder, array $options)
+        {
+            $builder->add('util_login', TextType :: class)
+                    ->add('util_mdp', TextType :: class)
+                    ->add('Connexion', SubmitType :: class);
+        }
+    }
+?>
