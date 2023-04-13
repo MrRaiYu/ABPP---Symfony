86266852854ee1f813866995a14f9c9f02eaaeab
Melvin Aubril <maubril28@gmail.com>
2023-04-13 11:26:16 +0200

    header avec variables de sessions

--- ABPP_Symfony/src/Controller/ConnexionController.php
+++ ABPP_Symfony/src/Controller/ConnexionController.php
@@ -8,75 +8,58 @@
     use Doctrine\Persistence\ManagerRegistry;
     use App\Formulaire\ConnexionType;
     use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
     class ConnexionController extends AbstractController
     {
         /**
-         * @Route("connexion", name="connexion")
-         */
-        function connexion(Request $requeteHTTP, ManagerRegistry $doctrine)
+        * @Route("connexion", name="connexion")
+        */
+        function connexion(Request $requestHTTP, ManagerRegistry $doctrine, SessionInterface $session)
         {
             $utilisateur = new Utilisateur();
             $formulaireUtilisateur = $this->createForm(ConnexionType :: class, $utilisateur);
-            $formulaireUtilisateur->handleRequest($requeteHTTP);
+            $formulaireUtilisateur->handleRequest($requestHTTP);
             if ($formulaireUtilisateur->isSubmitted() && $formulaireUtilisateur->isValid())
             {
                 $login = $formulaireUtilisateur['util_login']->getData();
                 $mdp = $formulaireUtilisateur['util_mdp']->getData();
                 $realmdp = $doctrine->getRepository(Utilisateur::class)->findOneBy(['UtilLogin' => $login , 'UtilMDP' => $mdp]);
+                
                 if ($realmdp == null)
                 {
-                    return new Response('Connexion échoué');
+                    return new Response('Utilisateur non existant');
                 }
-                else
+
+                $username = $realmdp->getUtilLogin();
+                $userRole = $realmdp->getProfil()->getProLib();
+                // Récupérer l'ID du rôle de l'utilisateur
+                $roleId = $realmdp->getProfil()->getId();
+
+                if ($roleId == null) {
+                    return new Response('Role non existant');
+                }
+                if($roleId == 1)
                 {
+                    // Stocker le username et le role dans une session
+                    $session->set('username', $username);
+                    $session->set('userRole', $userRole);
+
                     return $this->redirectToRoute("bofo");
+                }
+                if($roleId == 2)
+                {
+                    // Stocker le username et le role dans une session
+                    $session->set('username', $username);
+                    $session->set('userRole', $userRole);
+
+                    return $this->render('listEntreprise.html.twig');
                 }
             }
             else
             {
                 return $this->render('connexionform.html.twig', ['connexionform' => $formulaireUtilisateur->createView()]);
             }
-        }
-
-        /**
-         * @Route("bofo", name="bofo")
-         */
-        function bofo(Request $requeteHTTP, ManagerRegistry $doctrine)
-        {
-            return $this->render('bofo.html.twig');
-        }
-
-        /**
-         * @Route("choixGestion", name="choixGestion")
-         */
-        function choixGestion()
-        {
-            return $this->render('choixGestion.html.twig');
-        }
-
-        /**
-         * @Route("gestEntreprise", name="gestEntreprise")
-         */
-        function gestEntreprise()
-        {
-            return $this->render('gestEntreprise.html.twig');
-        }
-
-        /**
-         * @Route("gestUtil", name="gestUtil")
-         */
-        function gestUtil()
-        {
-            return $this->render('gestUtil.html.twig');
-        }
-
-        /**
-         * @Route("listEleves", name="listEleves")
-         */
-        function listEleves()
-        {
-            return new Response('Page listEleves');
         }
     }
 ?>
