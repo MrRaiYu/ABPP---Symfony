7081adbdf1ca2ce305e5e088acc2a0ec4e1b8965
MrRaiYu <jujuphil76@gmail.com>
2023-05-11 08:47:49 +0200

    specialite

--- ABPP_Symfony/src/Controller/entrepriseController.php
+++ ABPP_Symfony/src/Controller/entrepriseController.php
@@ -6,6 +6,7 @@
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use App\Entity\Entreprise;
     use Symfony\Component\HttpFoundation\Request;
+    use App\Entity\Specialite;
  
 
 
@@ -20,9 +21,19 @@
             $entityManager = $doctrine->getManager();
             $listeentreprises = $entityManager->getRepository(Entreprise::class)->findAll();
             $titre = 'Liste des entreprises';
+             // Initialiser un tableau pour stocker les spécialités de chaque entreprise
+            $specialitesEntreprises = [];
 
-            return $this->render('listeentreprises.html.twig' ,['Titre' => $titre, 'listeentreprises'=> $listeentreprises]);
-        }  
+        // Pour chaque entreprise, récupérer ses spécialités
+        foreach ($listeentreprises as $entreprise) {
+            $specialites = $entreprise->getSpecialites();
+            foreach ($specialites as $specialite) {
+                $specialiteentreprises[$entreprise->getId()][] = $specialite->getSpeLib();
+            }
+        }
+            
+            return $this->render('listeentreprises.html.twig' ,['Titre' => $titre, 'listeentreprises'=> $listeentreprises, 'specialiteentreprises'=> $specialiteentreprises]);
+        }
         
         /**
         *@Route("DetailEntreprise/{id}", name="DetailEntreprise")
@@ -30,10 +41,10 @@
 
         function DetailEntreprise($id, ManagerRegistry $doctrine) {
 
-             
+
             $entityManager = $doctrine->getManager();
             $listeentreprises = $doctrine->getRepository(Entreprise::class)->find($id);
-            return $this->render('DetailEntreprise.html.twig', ['entreprise' => $listeentreprises]);          
+            return $this->render('DetailEntreprise.html.twig', ['entreprise' => $listeentreprises]);
         }
 
         /**
@@ -57,28 +68,40 @@
             $listeentreprises = $doctrine->getRepository(Entreprise::class)->find($id);
 
             $formulaireEntreprise = $this->createForm(EntrepriseForm :: class, $listeentreprises);
+
+            // Récupérer les spécialités actuelles de l'entreprise
+            $specialites = $listeentreprises->getSpecialites()->toArray();
+
+             // Créer un formulaire pour modifier les spécialités de l'entreprise
+            $formulaireSpecialites = $this->createForm(EntrepriseForm::class, $specialites);
     
             $formulaireEntreprise->handleRequest($requestHTTP);
+            $formulaireSpecialites->handleRequest($requestHTTP);
 
 
-            if ($formulaireEntreprise->isSubmitted() && $formulaireEntreprise->isValid()) 
+            if ($formulaireEntreprise->isSubmitted() && $formulaireEntreprise->isValid() || $formulaireSpecialites->isSubmitted() && $formulaireSpecialites->isValid())
             {
 
                     $EntrepriseInfos = $formulaireEntreprise->getData();
-    
+      
                     $entityManager = $doctrine->getManager();
                     
                     $entityManager->persist($EntrepriseInfos);
                
                     $entityManager->flush();
                     
+                    // Mettre à jour les spécialités de l'entreprise
+                    $specialites = $formulaireSpecialites->getData();
+                    $entityManager = $doctrine->getManager();
+                    $entityManager->persist($listeentreprises);
+                    $entityManager->flush();
+
                    return $this->redirectToRoute('listeEntreprises');
             }
 
                     else
                     {
                     return $this->render('entrepriseform.html.twig' ,['entrepriseform' => $formulaireEntreprise->createView()]);
-                    }               
+                    }             
         }
-    }     
-?>
+    }
