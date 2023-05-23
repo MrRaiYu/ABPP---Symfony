fa999a98900a048f1010d020b0f63b677743c6bd
MrRaiYu <jujuphil76@gmail.com>
2023-05-23 14:59:39 +0200

    patch entreprise

--- ABPP_Symfony/src/Controller/entrepriseController.php
+++ ABPP_Symfony/src/Controller/entrepriseController.php
@@ -6,9 +6,6 @@
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use App\Entity\Entreprise;
     use Symfony\Component\HttpFoundation\Request;
-    use App\Entity\Specialite;
-    use App\Formulaire\RechercheEntreprise;
-    use Doctrine\DBAL\Query;
  
 
 
@@ -23,39 +20,9 @@
             $entityManager = $doctrine->getManager();
             $listeentreprises = $entityManager->getRepository(Entreprise::class)->findAll();
             $titre = 'Liste des entreprises';
-             // Initialiser un tableau pour stocker les spécialités de chaque entreprise
-            // $specialitesEntreprises = [];
 
-            // Pour chaque entreprise, récupérer ses spécialités
-            // foreach ($listeentreprises as $entreprise)
-            // {
-            //     $specialites = $entreprise->getSpecialites();
-            //     foreach ($specialites as $specialite)
-            //     {
-            //         $specialiteentreprises[$entreprise->getId()][] = $specialite->getSpeLib();
-            //     }
-            // }
-            $formulaireSearch = $this->createForm(RechercheEntreprise :: class);
-            $formulaireSearch->handleRequest($requestHTTP);
-            if ($formulaireSearch->isSubmitted() && $formulaireSearch->isValid())
-            {
-                $nom = $formulaireSearch['Nom']->getData();
-                $adresse = $formulaireSearch['Adresse']->getData();
-                $ville = $formulaireSearch['Ville']->getData();
-
-                //$listeentreprises = $doctrine->getRepository(Entreprise::class)->findBy(['EntRS' => '%' . $nom . '%']) OR $listeentreprises = $doctrine->getRepository(Entreprise::class)->findBy(['Adresse' => '%' . $adresse . '%']) OR $listeentreprises = $doctrine->getRepository(Entreprise::class)->findBy(['EntVille' => '%' . $ville . '%']);
-                $listeentreprises = $entityManager->getRepository(Entreprise::class)->RechercheEntreprise($nom,$ville,$adresse);
-                if ($listeentreprises==null)
-                {
-                   $this->addFlash('error', 'Aucune entreprise ne correspond à ces critères');
-                }
-                return $this->render('listeentreprises.html.twig' ,['listeentreprises'=> $listeentreprises, 'formulaireSearch' => $formulaireSearch->createView()]);
-            }
-            else
-            {
-                return $this->render('listeentreprises.html.twig' ,['listeentreprises'=> $listeentreprises, 'formulaireSearch' => $formulaireSearch->createView()]);
-            }
-        }
+            return $this->render('listeentreprises.html.twig' ,['Titre' => $titre, 'listeentreprises'=> $listeentreprises]);
+        }  
         
         /**
         *@Route("DetailEntreprise/{id}", name="DetailEntreprise")
@@ -63,64 +30,12 @@
 
         function DetailEntreprise($id, ManagerRegistry $doctrine) {
 
-
+             
             $entityManager = $doctrine->getManager();
             $listeentreprises = $doctrine->getRepository(Entreprise::class)->find($id);
-            return $this->render('DetailEntreprise.html.twig', ['entreprise' => $listeentreprises]);
+            return $this->render('DetailEntreprise.html.twig', ['entreprise' => $listeentreprises]);          
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
         /**
         *@Route("SupprimerEntreprise/{id}", name="SupprimerEntreprise")
         */
@@ -142,40 +57,28 @@
             $listeentreprises = $doctrine->getRepository(Entreprise::class)->find($id);
 
             $formulaireEntreprise = $this->createForm(EntrepriseForm :: class, $listeentreprises);
-
-            // Récupérer les spécialités actuelles de l'entreprise
-            $specialites = $listeentreprises->getSpecialites()->toArray();
-
-             // Créer un formulaire pour modifier les spécialités de l'entreprise
-            $formulaireSpecialites = $this->createForm(EntrepriseForm::class, $specialites);
     
             $formulaireEntreprise->handleRequest($requestHTTP);
-            $formulaireSpecialites->handleRequest($requestHTTP);
 
 
-            if ($formulaireEntreprise->isSubmitted() && $formulaireEntreprise->isValid() || $formulaireSpecialites->isSubmitted() && $formulaireSpecialites->isValid())
+            if ($formulaireEntreprise->isSubmitted() && $formulaireEntreprise->isValid()) 
             {
 
                     $EntrepriseInfos = $formulaireEntreprise->getData();
-      
+    
                     $entityManager = $doctrine->getManager();
                     
                     $entityManager->persist($EntrepriseInfos);
                
                     $entityManager->flush();
                     
-                    // Mettre à jour les spécialités de l'entreprise
-                    $specialites = $formulaireSpecialites->getData();
-                    $entityManager = $doctrine->getManager();
-                    $entityManager->persist($listeentreprises);
-                    $entityManager->flush();
-
                    return $this->redirectToRoute('listeEntreprises');
             }
 
                     else
                     {
                     return $this->render('entrepriseform.html.twig' ,['entrepriseform' => $formulaireEntreprise->createView()]);
-                    }             
+                    }               
         }
-    }
+    }     
+?>
